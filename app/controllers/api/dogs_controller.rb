43fdeb0b6fb6538a606dab08f9ba83400ee4f12c
Ambar <ambarmogg@me.com>
2020-11-18 19:25:38 -0800

    only allow create method if user is logged in

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,12 +1,16 @@
 class Api::DogsController < ApplicationController
 
   def create
+    if current_user
     @dog = Dog.new(
       name: params[:name],
       age: params[:age],
       breed: params[:breed]
     )
     @dog.save
-    render "show.json.jb"  
+    render "show.json.jb" 
+    else
+      render json: ["You are not allowed to create a dog without logging in"]
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :require_login
-
-  # private
-
-  # # def require_login
-  # #   unless logged_in?
-  # #     flash[:error] = "You must be logged in to create a new dog"
-  # #     # redirect_to new_login_url # halts request cycle
-  # #   end
-  # # end
 
   protect_from_forgery with: :exception, if: -> { request.format.html? }
 
